5f7f71a4116e64d586314bc9371fcffaf2484985
alexstrnik <alex.str.nik@gmail.com>
2025-02-20 13:32:04 +0300

    fix: register service condition

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,42 +1,41 @@
 import Cocoa
 import FlutterMacOS
 import ServiceManagement
-
 import UserNotifications
+
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    // https://github.com/leanflutter/window_manager/issues/214
-    return false
-  }
-    
-  override func applicationDidFinishLaunching(_ aNotification: Notification) {
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        // https://github.com/leanflutter/window_manager/issues/214
+        return false
+    }
+
+    override func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Request notification authorization
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge]) { granted, error in
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge]) {
+            granted, error in
             if let error = error {
                 print("Error requesting notification authorization: \(error)")
             }
         }
-      
-      if #available(macOS 13.0, *) {
-          let service = SMAppService.daemon(plistName: "app.hiddify.com.daemon.plist")
-          
-          let status = service.status
-          
-          if service.status == .notRegistered {
-              do {
-                  try service.register()
-              } catch {
-                  print(error)
-              }
-          }
-      }
+
+        if #available(macOS 13.0, *) {
+            let service = SMAppService.daemon(plistName: "app.hiddify.com.daemon.plist")
+
+            if service.status != .enabled {
+                do {
+                    try service.register()
+                } catch {
+                    print(error)
+                }
+            }
+        }
     }
-    
+
     override func applicationWillTerminate(_ notification: Notification) {
         if #available(macOS 13.0, *) {
             let service = SMAppService.daemon(plistName: "app.hiddify.com.daemon.plist")
-            
+
             do {
                 try service.unregister()
             } catch {
@@ -44,18 +43,18 @@ class AppDelegate: FlutterAppDelegate {
             }
         }
     }
-    
-  // // window manager restore from dock: https://leanflutter.dev/blog/click-dock-icon-to-restore-after-closing-the-window
-  // override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
-  //     if !flag {
-  //         for window in NSApp.windows {
-  //             if !window.isVisible {
-  //                 window.setIsVisible(true)
-  //             }
-  //             window.makeKeyAndOrderFront(self)
-  //             NSApp.activate(ignoringOtherApps: true)
-  //         }
-  //     }
-  //     return true
-  // }
+
+    // // window manager restore from dock: https://leanflutter.dev/blog/click-dock-icon-to-restore-after-closing-the-window
+    // override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+    //     if !flag {
+    //         for window in NSApp.windows {
+    //             if !window.isVisible {
+    //                 window.setIsVisible(true)
+    //             }
+    //             window.makeKeyAndOrderFront(self)
+    //             NSApp.activate(ignoringOtherApps: true)
+    //         }
+    //     }
+    //     return true
+    // }
 }
